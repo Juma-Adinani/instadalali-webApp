1e50b155b82293ce51c87243fd0e9fb6c37870ee
Paschal Giki <paschalgiki@gmail.com>
2022-01-16 15:55:19 +0300

    Build v4

---
+++ build/precache-manifest.2d8663d906c0ab58612288e24c241007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45349556cf979bba190abaa6e5d4f2f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ddd2a1f3fef81eaed4a",
+    "url": "/static/css/2.d2864bb6.chunk.css"
+  },
+  {
+    "revision": "8ddd2a1f3fef81eaed4a",
+    "url": "/static/js/2.e2790e4b.chunk.js"
+  },
+  {
+    "revision": "6a28db7101afcfef00c612f49e6ffdf7",
+    "url": "/static/js/2.e2790e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d87f31e103f735a7dad",
+    "url": "/static/js/main.6643434c.chunk.js"
+  },
+  {
+    "revision": "b8b17fff3ffca653eacd",
+    "url": "/static/js/runtime-main.aa4a40ec.js"
+  }
+]);
